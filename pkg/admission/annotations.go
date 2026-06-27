dba5c48a577bb345660274239a874395222f57d0
Yonatan Kahana <yonatankahana.il@gmail.com>
2026-06-27 16:44:03 +0300

    chore: update license date for pkg/admission/annotations.go

--- pkg/admission/annotations.go
+++ pkg/admission/annotations.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2021 Yonatan Kahana
+Copyright © 2026 Yonatan Kahana
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
